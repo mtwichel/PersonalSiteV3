5a96de5d9791bf59f4def3f75b0b629d65f78a3b
Marcus Twichel <mtwichel@Marcuss-MacBook-Pro.local>
2018-10-04 02:35:50 -0600

    finished project cards

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { HttpClient } from '@angular/common/http';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-project-card',
@@ -10,8 +12,22 @@ export class ProjectCardComponent implements OnInit {
 
   @Input() project;
 
-  constructor(db: AngularFirestore) { }
+  dateString: string;
 
-  ngOnInit() { }
+  constructor(private db: AngularFirestore, private http: HttpClient, private storage: AngularFireStorage) { }
 
+  ngOnInit() {
+    if (this.project.githubRepo) {
+      this.getGithubLastUpdated();
+    }
+  }
+
+  getGithubLastUpdated() {
+    this.http.get(`https://api.github.com/repos/mtwichel/${this.project.githubRepo}/commits`).subscribe(obj => {
+      const dateString = obj[0].commit.author.date;
+      const date = new Date(dateString);
+      console.log(date.toString());
+      this.dateString = `${date.getMonth() + 1}.${date.getDate()}.${date.getFullYear().toString().substring(2)}`;
+    });
+  }
 }
